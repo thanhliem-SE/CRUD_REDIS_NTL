a0d4f2fb765c5c73056c8c33439081354c6e547d
thanhliem-SE <thanhliem2n@gmail.com>
2021-11-08 10:24:31 +0700

    commit crud hash

---
+++ src/test/java/com/example/crud_redis_ntl/CrudRedisNtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_ntl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisNtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
